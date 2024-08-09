761dd7cc37ace5aa32d89ec01b3d84f3d6446ad0
Wisnoe <wisnoemf@Wisnoes-MacBook-Pro.local>
2024-08-09 08:17:45 +0700

    ✨ Seeder

--- database/seeds/DataMaster.php
+++ database/seeds/DataMaster.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DataMaster extends Seeder
 {
@@ -12,19 +13,24 @@ class DataMaster extends Seeder
     public function run()
     {
         // DB::insert('INSERT INTO pasiens (nik,nama,umur,jenis_kelamin,alamat,odp_date,status) values '.$values.' ');
-        // INSERT INTO barangs (kode, nama , harga)
-        // VALUES
-        // ('A001', 'Barang A', 200000),
-        // ('C025', 'Barang B', 350000),
-        // ('A102', 'Barang C', 125000),
-        // ('A301', 'Barang D', 350000),
-        // ('B221', 'Barang E', 125000);
-        // INSERT INTO customers (kode, name , telp)
-        // VALUES
-        // ('C01', 'Cust A', '088888888'),
-        // ('C02', 'Cust B', '088888888'),
-        // ('C03', 'Cust C', '088888888'),
-        // ('C04', 'Cust D', '088888888'),
-        // ('C05', 'Cust E', '088888888');
+
+
+        // Insert data into 'barangs' table
+        DB::table('barangs')->insert([
+            ['kode' => 'A001', 'nama' => 'Barang A', 'harga' => 200000],
+            ['kode' => 'C025', 'nama' => 'Barang B', 'harga' => 350000],
+            ['kode' => 'A102', 'nama' => 'Barang C', 'harga' => 125000],
+            ['kode' => 'A301', 'nama' => 'Barang D', 'harga' => 350000],
+            ['kode' => 'B221', 'nama' => 'Barang E', 'harga' => 125000],
+        ]);
+
+        // Insert data into 'customers' table
+        DB::table('customers')->insert([
+            ['kode' => 'C01', 'name' => 'Cust A', 'telp' => '088888888'],
+            ['kode' => 'C02', 'name' => 'Cust B', 'telp' => '088888888'],
+            ['kode' => 'C03', 'name' => 'Cust C', 'telp' => '088888888'],
+            ['kode' => 'C04', 'name' => 'Cust D', 'telp' => '088888888'],
+            ['kode' => 'C05', 'name' => 'Cust E', 'telp' => '088888888'],
+        ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserSeeder::class);
         $this->call(LaratrustSeeder::class);
+        $this->call(DataMaster::class);
     }
 }
